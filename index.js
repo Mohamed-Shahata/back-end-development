a9b0a5925d53324a9e5481b30dd84e39cf818b16
Mohamed-Shahata <mohamedmrslan@gmail.com>
2025-09-03 16:52:24 -0100

    edit of index.js file

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import e from "express";
+import path from "path";
 
 const app = e();
 const PORT = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
+app.get("/package.json", (req, res) => {
+  res.sendFile(path.join(__dirname, "package.json"));
 });
 
 app.listen(PORT, () => console.log(`Server is live on port ${PORT}`));
